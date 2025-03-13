070ee22605a69deeb70e0fc243dfab0e992f33d7
DiamondraAndriam <diamondra.andriamampiandra@gmail.com>
2025-03-13 18:27:56 +0300

    Update config for deployment

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-const express = require('express');
-const app = express();
+const express = require('express'); 
+const mongoose = require('mongoose'); 
+const cors = require('cors'); 
+require('dotenv').config(); 
+const app = express(); 
+const PORT = process.env.PORT || 5000; 
 
-const PORT = 3000;
+// Middleware 
+app.use(cors()); 
+app.use(express.json()); 
 
+// Connexion à MongoDB 
+mongoose.connect(process.env.MONGO_URI, { 
+useNewUrlParser: true, 
+useUnifiedTopology: true 
+}).then(() => console.log("MongoDB connecté")) 
+  .catch(err => console.log(err)); 
+
+// Routes
 app.get('/', (req, res) => {
-    res.send('Hello, Express!');
+  res.json({ message : 'Hello, Express!'});
 });
 
-app.listen(PORT, () => {
-    console.log(`Serveur démarré sur http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Serveur démarré sur le port 
+${PORT}`));
