332ff62150b11a159e83495d02ac10a7ddaa47a8
DiamondraAndriam <diamondra.andriamampiandra@gmail.com>
2025-03-14 23:51:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express'); 
 const mongoose = require('mongoose'); 
-//const cors = require('cors'); 
+const cors = require('cors'); 
 require('dotenv').config(); 
 const app = express(); 
 const PORT = process.env.PORT || 5000; 
 
 // Middleware 
-//app.use(cors()); 
-//app.use(express.json()); 
+/*app.use(cors()); 
+app.use(express.json()); 
 
 // Connexion à MongoDB 
 mongoose.connect(process.env.MONGO_URI, { 
@@ -15,6 +15,7 @@ useNewUrlParser: true,
 useUnifiedTopology: true 
 }).then(() => console.log("MongoDB connecté")) 
   .catch(err => console.log(err)); 
+*/
 
 // Routes
 app.get('/', (req, res) => {
