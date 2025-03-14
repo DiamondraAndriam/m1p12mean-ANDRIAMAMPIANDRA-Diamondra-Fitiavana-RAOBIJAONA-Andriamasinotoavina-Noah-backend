3c06a66cec8abc3951ad0216bdb16096b99b5953
DiamondraAndriam <diamondra.andriamampiandra@gmail.com>
2025-03-15 00:00:44 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,38 @@
-const express = require('express'); 
-const mongoose = require('mongoose'); 
-const cors = require('cors'); 
-require('dotenv').config(); 
-const app = express(); 
-const PORT = process.env.PORT || 5000; 
+// const express = require('express'); 
+// const mongoose = require('mongoose'); 
+// const cors = require('cors'); 
+// require('dotenv').config(); 
+// const app = express(); 
+// const PORT = process.env.PORT || 5000; 
 
-// Middleware 
-app.use(cors()); 
-app.use(express.json()); 
+// // Middleware 
+// app.use(cors()); 
+// app.use(express.json()); 
 
-// Connexion à MongoDB 
-mongoose.connect(process.env.MONGO_URI, { 
-useNewUrlParser: true, 
-useUnifiedTopology: true 
-}).then(() => console.log("MongoDB connecté")) 
-  .catch(err => console.log(err)); 
+// // Connexion à MongoDB 
+// mongoose.connect(process.env.MONGO_URI, { 
+// useNewUrlParser: true, 
+// useUnifiedTopology: true 
+// }).then(() => console.log("MongoDB connecté")) 
+//   .catch(err => console.log(err)); 
 
-// Routes
+// // Routes
+// app.get('/', (req, res) => {
+//   res.json({ message : 'Hello, Express!'});
+// });
+
+// app.listen(PORT, () => console.log(`Serveur démarré sur le port 
+// ${PORT}`));
+
+// module.exports = app;
+
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
-  res.json({ message : 'Hello, Express!'});
+    res.send('Hello, Express on Vercel Test!');
 });
-
-app.listen(PORT, () => console.log(`Serveur démarré sur le port 
-${PORT}`));
 
 module.exports = app;
