2654450aa8aec6397ec865ea69fa7c5e6894c313
DiamondraAndriam <diamondra.andriamampiandra@gmail.com>
2025-03-14 23:44:10 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ app.use(cors());
 app.use(express.json()); 
 
 // Connexion à MongoDB 
-mongoose.connect(process.env.MONGO_URI, { 
+/*mongoose.connect(process.env.MONGO_URI, { 
 useNewUrlParser: true, 
 useUnifiedTopology: true 
 }).then(() => console.log("MongoDB connecté")) 
   .catch(err => console.log(err)); 
-
+*/
 // Routes
 app.get('/', (req, res) => {
   res.json({ message : 'Hello, Express!'});
