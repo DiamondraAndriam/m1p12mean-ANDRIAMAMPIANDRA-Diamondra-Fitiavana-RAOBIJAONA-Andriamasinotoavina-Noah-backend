3eeea155bed4b256ce6786595bc5cb74c7c7a817
NoahRaw <raobijaonanoah3@gmail.com>
2025-03-18 06:50:58 +0300

    FEAT: add user authentication

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
-const express = require('express'); 
-const mongoose = require('mongoose'); 
-const cors = require('cors'); 
-require('dotenv').config(); 
-const app = express(); 
-const PORT = process.env.PORT || 5000; 
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const authRoutes = require('./src/routes/auth.routes');
+const cors = require('cors');
 
-// Middleware 
-app.use(cors()); 
-app.use(express.json()); 
+dotenv.config();
 
-// Connexion à MongoDB 
-mongoose.connect(process.env.MONGO_URI, { 
-useNewUrlParser: true, 
-useUnifiedTopology: true 
-}).then(() => console.log("MongoDB connecté")) 
-  .catch(err => console.log(err)); 
+connectDB();
 
-// Routes
-app.get('/', (req, res) => {
-  res.json({ message : 'Hello, Express!'});
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
-app.listen(PORT, () => console.log(`Serveur démarré sur le port 
-${PORT}`));
+module.exports = app;
