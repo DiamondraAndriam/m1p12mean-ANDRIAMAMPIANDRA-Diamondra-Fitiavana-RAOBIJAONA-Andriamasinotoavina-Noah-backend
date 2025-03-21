e0e3a578554f6529e02e4f09e373701f74a43344
NoahRaw <raobijaonanoah3@gmail.com>
2025-03-22 00:29:11 +0300

    fix: server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
@@ -17,9 +16,13 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Welcome to MEAN backend');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port http://localhost:${PORT}`);
 });
 
 module.exports = app;
